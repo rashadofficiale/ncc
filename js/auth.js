36205117095ab2982a94250b709ae4fe393db8f9
ahmadrashad647-dot <ahmadrashad647@gmail.com>
2026-02-03 01:26:59 +0530

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { firebaseConfig } from "./firebase-config.js";
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const loginBtn = document.getElementById("loginBtn");
+const signupBtn = document.getElementById("signupBtn");
+
+// LOGIN
+loginBtn.addEventListener("click", () => {
+  signInWithEmailAndPassword(auth, emailInput.value, passwordInput.value)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch(err => alert(err.message));
+});
+
+// SIGNUP
+signupBtn.addEventListener("click", () => {
+  createUserWithEmailAndPassword(auth, emailInput.value, passwordInput.value)
+    .then(() => {
+      alert("Account created. Now login.");
+    })
+    .catch(err => alert(err.message));
+});
