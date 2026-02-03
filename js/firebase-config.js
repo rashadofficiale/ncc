359cf69beb4f2b3a28d41b427de0bfc29ea3f8bd
ahmed <ahmadrashad647@gmail.com>
2026-02-04 00:26:33 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,13 @@
 // Firebase CDN v8
 var firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyCrlP360Fpx-9tCyggJeoqLgkwnTlRTJ5o",
   authDomain: "ncc-project-3b0f1.firebaseapp.com",
   projectId: "ncc-project-3b0f1",
-  storageBucket: "ncc-project-3b0f1.appspot.com",
+  storageBucket: "ncc-project-3b0f1.firebasestorage.app",
   messagingSenderId: "590441541895",
   appId: "1:590441541895:web:2a861830fcb60f09f655a2"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 
